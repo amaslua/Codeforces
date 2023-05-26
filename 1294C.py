88b3cf8783a54739d06800829c4e1690ffda2a4e
amaslua <amaslua@gmail.com>
2023-05-26 23:59:32 +0800

    260523

---
+++ 1294C.py
@@ -0,0 +1,26 @@
+x = int(input())
+
+for _ in range(x):
+    n = int(input())
+    f = []
+    used = []
+    for _ in range(2):
+        x = 2
+        while x < n:
+            if n % x == 0 and x not in used:
+                f.append(x)
+                used.append(x)
+                n = n/x
+                break
+            else:
+                x += 1
+    f.append(int(n))
+    if len(f) == 3 and len(set(f)) == 3:
+        print('YES')
+        out = ""
+        for i in f:
+            out += str(i)
+            out += " "
+        print(out[:-1])
+    else:
+        print('NO')
