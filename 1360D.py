403a22da16c923a671f47c482cae063d5ef0d09f
amaslua <amaslua@gmail.com>
2023-06-11 23:47:01 +0800

    Edited 1360D.py

--- 1360D.py
+++ 1360D.py
@@ -4,13 +4,17 @@ cases = int(input())
 
 for _ in range(cases):
     n, k = list(map(int, input().split(" ")))
-    n_copy = n
+#    print(min(min(n//i,n if n//i>k else i)
+#    for i in range(1,min(int(n**.5),k)+1)if n%i==0))
     if k > n:
         print(1)
     elif k == 1:
         print(n)
     else:
-        for i in range(min(int(math.sqrt(n)), k), 1, -1):
+        ans = 1
+        for i in range(1, 1 + min(int(n**0.5), k)):
             if n % i == 0:
-                print(min(int(n/i), i))
-                break
+                if n/i <= k:
+                    ans = max(ans, n/i)
+                ans = max(ans, i)
+        print(int(n/ans))
